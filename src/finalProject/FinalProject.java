296028773fa592196f2c12fd863a1c378e258a8f
alonso-herreros <alonso.herreros.c@gmail.com>
2022-12-09 16:12:16 +0100

    Fully functional (until proven otherwise)

--- src/finalProject/FinalProject.java
+++ src/finalProject/FinalProject.java
@@ -2,21 +2,19 @@ package finalProject;
 import java.util.Scanner;
 
 public class FinalProject {
-	//TODO
-	//Recordar por que en algunos casos le resto '1' como por ejemplo en interpretationofstringmovement
-	//Relativos
 
 	static Scanner input = new Scanner(System.in);//*STATIC
 
-	//The variable that is going to be created can be used by any method (function) of this class
+	//The variables that are going to be created as static can be used by any method (function) of this class
 	static char[][] charsInBoard = new char [8][8];
 	static int turn;
-	static int mode;//Depending on the number that the user writes it will have a different value (1,2 or 3)
-	//Creation of a new method that is going to populate charsInBoard following the rules given
-
+	static int mode;//Depending on the number that the user writes, it will have a different value (1,2 or 3)
+	static int [] last2Coordinates=new int[2];//Static because it is going to be used in different functions of this class (For example in huffing and main)
+	//Creation of a new method that is going to populate charsInBoard following the rules given.
+	static boolean movementOfCapture=false;
 	public static void initializeCharsOfBoard() { //The () is empty because it doesn't depend on anything.This method is just going to show the initial board
 		//but it is not going to do another function.
-		//We are going to do 2 fors so we can access to all the 64 cells (8x8 matrix=64 cells).
+		//We are going to do 2 for-loops so we can access to all the 64 cells (8x8 matrix=64 cells).
 		for (int i=0; i<charsInBoard.length; i++) {
 			for (int j=0; j<charsInBoard[i].length; j++) {
 				if ((i+j)%2==0) { //For alternating squares, being i+j even
@@ -42,9 +40,10 @@ public class FinalProject {
 		turn = 1;
 		int thereIsAWinner=0;//It starts from 0 as at first there is no winner.If its final value is 1, means that the white pieces have won, and if its
 		//final value is 2, it means that the black pieces have won.
-
+		String errorMessage="";//Empty because at first we don't want to say the player that they (he/she) have done something wrong without being true.
 		do {//Repeats turns until there is a winner
 			boolean nextTurn=false;
+
 			boolean hasMadeAMovement=false;
 			String movement; //Ask for a movement
 			boolean validMovement;//Check if it's valid
@@ -57,28 +56,65 @@ public class FinalProject {
 				}else {
 					System.out.println("BLACK'S PIECES WIN!!!!!!");
 				}
-				break;//If there is a winner is 0, it will go out of the if as there is no winner.
+				break;//If there is a winner is 0, it will go out of the if as there is no winner and they have to keep playing.
 			}
 
+
 			do {//Loop until there's a change of turn
-				do {//Until there is a valid movement or the player end the turn.
-					drawBoard();
+
+				do {//Until there is a valid movement or the player ends the turn.
+
+					drawBoard(); //For each movement , the board is updated, it is going to show that new movement.
+					System.out.print(errorMessage); //Because we don't want to show any error message, it has to be empty
+					errorMessage="";
+
 					movement = askForMovement(); //askForMovement asks for a movement and return that movement. That's why movement = askForMovement.
-					validMovement = checkFormatOfMovement(movement);
+					validMovement = checkFormatOfMovement(movement);//checkFormatOfMovement checks if that movement is valid and the result is stored in the validMovement variable.
 
 					if ((validMovement)) { //If not valid movement, goes to while and as it 's still not valid, a new movement will be asked.
 						coordinates=interpretationOfStringMovementAsNumbers(movement); //coordinates is going to hold the position and the movement as numbers.This conversion has been done in the interpretationOfStringMovementAsNumbers function
-						validMovement=validMovementInTheBoard(coordinates);
+						validMovement=validMovementInTheBoard(coordinates);//If the coordinates given by the player are also valid in the board, the result of if it's valid or not is stored in the validMovement variable
+
+						if(hasMadeAMovement==true) {
+							if((coordinates[0]!=last2Coordinates[0])||(coordinates[1]!=last2Coordinates[1])) {
+								validMovement=false;
+								errorMessage = "You can't move a different piece!!!! ";
+							}
+							if(movement.length()==10){
+								validMovement=false;
+								errorMessage = "If you can keep capturing you MUST do it. If not, end your turn (press enter) ";
+							}
+						}
+
 						if(validMovement) {
 							makeTheMovement(coordinates);
+							if(movement.length()==10) {
+								last2Coordinates[0]=coordinates[2];
+								last2Coordinates[1]=coordinates[3];
+							}else {
+								last2Coordinates[0]=coordinates[4];
+								last2Coordinates[1]=coordinates[5];
+							}
 							hasMadeAMovement=true;//If it's a valid movement and the player has made a movement , it will be drawn in the board
 							if(movement.length()==10) {//If the player has only move the piece but not capture an opponent's piece, his turn ends.
 								nextTurn=true;
 							}
+						}else if(errorMessage.length()==0){//If there wasn't before an error message or if we hadn't specified why is wrong. It will happen...
+							errorMessage="Not a valid movement ";
 						}
-					}else if (hasMadeAMovement==true){ //If it's not a valid movement and the player has made a movement (which is required)
-						nextTurn=endOfTurn(movement);// it checks the function endOfTurn.
+					}else {
+						if ((movement.equalsIgnoreCase("Huffing"))&&(hasMadeAMovement=false)) { //If the player has written huffing without doing before any movement
+							huffing();
+							if(huffing()==true) {
+								charsInBoard[last2Coordinates[0]][last2Coordinates[1]]=' ';//If there is huffing, the piece that was moved in the last turn (whose current position is the last coordinates given in the previous turn(which has been the last one because the current player first said 'huffing' instead of moving ) is now eliminated, so its value is empty
+							}
+						}else if((endOfTurn(movement)==true)&&(hasMadeAMovement==true)){
+							nextTurn=true;//to enter in the if, endOfTurn must be true (and hasMadeAMovement is true), so nextTurn is also true.
+						}else {
+							errorMessage = "That's not an option!!!! ";
+						}
 					}
+
 					//What does this "do-while" do? First the program asks for a movement to the user. That movement is stored in the variable movement,
 					//which we have declared before the loop that is a string. Then, using the method checkFormatOfMovement, checks that the movement given by the user
 					//is valid or not, and it is stored in the variable validMovement, which is a boolean because we declared it before the loop and it's going to say if that
@@ -96,10 +132,7 @@ public class FinalProject {
 			}else if(turn==2) {
 				turn=1;
 			}
-			//TODO
-			//mensajes para jugadores*20m VIERNES
 
-			//huffing*1:30h
 		}while(thereIsAWinner==0);//The loop will be repeated while thereIsAWinner is 0, which means while there is no winner.
 	}
 	public static void drawBoard() {
@@ -138,7 +171,7 @@ public class FinalProject {
 	public static void selectMode() {
 		// Mode selection
 		// 1 - basic mode
-		// 2 - intermediate(queens)
+		// 2 - intermediate(kings)
 		// 3 - advanced(huffing)
 		System.out.println("CHOOSE A GAME OPTION:");
 		System.out.println("1)Basic Mode");
@@ -160,7 +193,7 @@ public class FinalProject {
 		}else {
 			System.out.println("It's the black player's turn. ");
 		}
-		System.out.println("Make your movement ! \n Remember to do it in this form (row1,col1)(row2,col2)"); //(row1,col1) is the initial position of the piece and
+		System.out.println("Make your movement ! \n Remember to do it in this form (row1,col1)(row2,col2).If you want to huff , write: huffing "); //(row1,col1) is the initial position of the piece and
 		//(row2,col2) is the final position of the piece.
 		String movement = input.nextLine(); //Read the movement of the player
 		return movement; //Return the movement to the main method, that will then be stored in the variable called movement of the main method
@@ -198,7 +231,12 @@ public class FinalProject {
 		relative [0] = coordinates [2] - coordinates [0]; //i
 		relative [1] = coordinates [3] - coordinates [1]; //j
 
-
+		for(int p=0;p<coordinates.length;p++) {
+			//Look for the 6 numbers that form the coordinates.If all of them are between 0 and 7 it will continue checking the rest of the conditions. If not, it will return false, so that movement is not valid in the board
+			if((coordinates[p]<0)||(coordinates[p]>7))  {
+				return false;
+			}
+		}
 		if (whosePiece(coordinates[0],coordinates[1])!=1){ //If it's not the piece of the player who is playing it will return false
 			return false ;
 		}
@@ -263,18 +301,24 @@ public class FinalProject {
 		if (whosePiece(coordinates[2],coordinates[3])==2) { //This means that the player is going to capture an opponent's piece
 			charsInBoard[coordinates[2]][coordinates[3]]=' '; //The position where there was the opponent's piece is not empty (because the player has capture that piece)
 			charsInBoard[coordinates[4]][coordinates[5]]=charsInBoard[coordinates[0]][coordinates[1]]; //In the final position of the piece that has captured the opponent's piece, there must appear the piece which was at the initial position before capturing the opponent's piece
-			queenPiece(coordinates[4],coordinates[5]);//If the player captures an opponent's piece and arrives at the initial row of the opponent's pieces, it will be converted into a queen.
-			//In this case, as the final coordinates are the 4 and 5 ones , the function queenPiece has to only consider those coordinates in order to convert the pieces into queens, depending on if they are w or b.
+			movementOfCapture=true;//If it's a movement in which the player captures an opponent's piece, it has been made a movement of capture so its value is true
+			if (mode>1) {//Because if mode is 1 the pieces are not converted into kings
+				kingPiece(coordinates[4],coordinates[5]);//If the player captures an opponent's piece and arrives at the initial row of the opponent's pieces, it will be converted into a king.
+			}
+			//In this case, as the final coordinates are the 4 and 5 ones , the function kingPiece has to only consider those coordinates in order to convert the pieces into kings, depending on if they are w or b.
 		}else {
 			charsInBoard[coordinates[2]][coordinates[3]]=charsInBoard[coordinates[0]][coordinates[1]];
-			queenPiece(coordinates[2],coordinates[3]);//If the player captures an opponent's piece and arrives at the initial row of the opponent's pieces, it will be converted into a queen.
-			//In this case, as the final coordinates are the 2 and 3 ones , the function queenPiece has to only consider those coordinates in order to convert the pieces into queens, depending on if they are w or b.
+			movementOfCapture=false;//If it's a movement in which the player doesn't capture an opponent's piece, it has not been made a movement of capture so its value is false
+			if (mode>1) {//Because if mode is 1 the pieces are not converted into kings
+				kingPiece(coordinates[2],coordinates[3]);//If the player captures an opponent's piece and arrives at the initial row of the opponent's pieces, it will be converted into a king.
+			}
+			//In this case, as the final coordinates are the 2 and 3 ones , the function kingPiece has to only consider those coordinates in order to convert the pieces into kings, depending on if they are w or b.
 		}
 		charsInBoard[coordinates[0]][coordinates[1]]=' '; //The piece which was at the initial position, as it has been moved, in that position now there is nothing.
 
 	}
-	public static void queenPiece(int i,int j) {
-		//Check if the piece has to be converted into a queen piece and then it converts it.
+	public static void kingPiece(int i,int j) {
+		//Check if the piece has to be converted into a king piece and then it converts it.
 		if ((charsInBoard[i][j]=='b')&&(i==7)){
 			charsInBoard[i][j]='B';
 		}else if ((charsInBoard[i][j]=='w')&&(i==0)) {
@@ -289,7 +333,6 @@ public class FinalProject {
 		}
 	}
 	public static int checkWinner() {
-		boolean movementInRange=true;
 		boolean winsBlackPieces=true;
 		boolean winsWhitePieces=true;
 
@@ -301,41 +344,12 @@ public class FinalProject {
 
 		for(int i=0;i<charsInBoard.length;i++) {
 			for(int j=0;j<charsInBoard[i].length;j++) {
-				int[][] possibleCoordinatesForMovement= {
-						{i,j,i+1,j+1,0,0},
-						{i,j,i-1,j-1,0,0},
-						{i,j,i+1,j-1,0,0},
-						{i,j,i-1,j+1,0,0},
+				if((canCapture(i,j)==true)||(canMove(i,j)==true)) {
+					if((charsInBoard[i][j]=='b')||(charsInBoard[i][j]=='B')) {
+						winsWhitePieces = false;
 
-						{i,j,i+1,j+1,i+2,j+2},
-						{i,j,i-1,j-1,i-2,j-2},
-						{i,j,i+1,j-1,i+2,j-2},
-						{i,j,i-1,j+1,i-2,j+2},
-				};
-				for(int k=0;k<possibleCoordinatesForMovement.length;k++) {
-					for(int p=0;p<possibleCoordinatesForMovement[k].length;p++) {
-						//For each k it is going to check if all the p values are between the range that has the board
-						if((possibleCoordinatesForMovement[k][p]>=0)&&(possibleCoordinatesForMovement[k][p]<=7)) {
-							movementInRange=true;
-						}else { //If it's not between the range of the board
-							movementInRange=false;
-							break;//It gets out of the loop that depends on p
-						}
-
-					}
-					if(movementInRange==true){//If the movement is between the range , it will be checked in the validMovementInTheBoard function.
-						//If in any value of p there is a movement that it's not between the range of the board,the entire movement can't be done, it doesn't go to the validMovementInBoard function to be checked because
-						//it is already known that it is not valid.
-						validMovementInTheBoard(possibleCoordinatesForMovement[k]);
-						if(validMovementInTheBoard(possibleCoordinatesForMovement[k])==true) {
-							if((charsInBoard[i][j]=='b')||(charsInBoard[i][j]=='B')) {
-								winsWhitePieces = false;
-
-							}else {
-								winsBlackPieces = false;
-							}
-
-						}
+					}else {
+						winsBlackPieces = false;
 					}
 				}
 
@@ -350,10 +364,79 @@ public class FinalProject {
 			return 0;
 		}
 	}
+	public static boolean huffing() {
+		//Ha utilizado una pieza para comer y ha pasado de turno pero podría haber seguido comiendo
+		//Podria haber utilizado otra pieza para comer pero no ha comido
+		//To check if the previous player could have moved a piece, we have to come back to their turn in order to check their validity of their movement
+		if(turn==1) {
+			turn=2;
+		}else if(turn==2) {
+			turn=1;
+		}
+		if((movementOfCapture==true)&&(canCapture(last2Coordinates[0],last2Coordinates[1])==true)) {
+			//Change again the turn to the player who has to move next . (Because the first change of player has been done before in order to check if their movement was valid or not).
+			if(turn==1) {
+				turn=2;
+			}else if(turn==2) {
+				turn=1;
+			}
+			return true;
+		}else if(movementOfCapture==false){
+			for(int i=0;i<charsInBoard.length;i++) {
+				for(int j=0;j<charsInBoard[i].length;j++) {
+
+					if((whosePiece(i,j)==1)&&(canCapture(i,j)==true)) {
+						//Change again the turn to the player who has to move next . (Because the first change of player has been done before in order to check if their movement was valid or not).
+						if(turn==1) {
+							turn=2;
+						}else if(turn==2) {
+							turn=1;
+						}
+						return true;
+					}
+				}
+			}
+
+		}
+		//Change again the turn to the player who has to move next . (Because the first change of player has been done before in order to check if their movement was valid or not).
+		if(turn==1) {
+			turn=2;
+		}else if(turn==2) {
+			turn=1;
+		}
+		return false; //If it doesn't check any of the previous conditions, it's not huffing.
 
 
+	}
+	public static boolean canCapture(int i,int j) {
+		int[][] possibleCoordinatesForCapture= {
 
+				{i,j,i+1,j+1,i+2,j+2},
+				{i,j,i-1,j-1,i-2,j-2},
+				{i,j,i+1,j-1,i+2,j-2},
+				{i,j,i-1,j+1,i-2,j+2},
+		};
+		for(int k=0;k<possibleCoordinatesForCapture.length;k++) {
+			//If it's a valid movement in the board it will be checked in that function
+			if(validMovementInTheBoard(possibleCoordinatesForCapture[k])==true) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean canMove(int i,int j) {
+		int[][] possibleCoordinatesForMovement= {
+				{i,j,i+1,j+1,0,0},
+				{i,j,i-1,j-1,0,0},
+				{i,j,i+1,j-1,0,0},
+				{i,j,i-1,j+1,0,0},
+		};
+		for(int k=0;k<possibleCoordinatesForMovement.length;k++) {
+			if(validMovementInTheBoard(possibleCoordinatesForMovement[k])==true) {
+				return true;
+			}
+		}
+		return false;
 
-
-
-} 
+	}
+}
